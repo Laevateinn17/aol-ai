408bfa7ad3bf6ef30f234e0a9704b52323bc012b
Laevateinn17 <vincentramaputra123@gmail.com>
2024-01-04 20:56:19 +0700

    secondd

--- app.py
+++ app.py
@@ -13,24 +13,33 @@ CORS(app)
 bluejack_url = 'https://bluejack.binus.ac.id/lapi/api/Assistant/All'
 messier_url = 'https://socs1.binus.ac.id/messier/GeneralWeb.svc/GetThumbnail/'
 
+def remove_punctuation(text):
+    import string
+    translator = str.maketrans("", "", string.punctuation)
+    return text.translate(translator)
+
+def remove_stopwords(words):
+    stop_words = set(stopwords.words("indonesian"))
+    return [word for word in words if word.lower() not in stop_words]
+
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello world'
+    return 'Hello there'
 
-@app.route('/assistants', methods=['GET'])
-def assistant():
-    response = requests.get(bluejack_url)
-    if response.status_code == 200:
-        data = response.json()
-        list = data.get('active', []) + data.get('inactive', [])
+# @app.route('/assistants', methods=['GET'])
+# def assistant():
+#     response = requests.get(bluejack_url)
+#     if response.status_code == 200:
+#         data = response.json()
+#         list = data.get('active', []) + data.get('inactive', [])
         
-        for value in list:
-            picture_id = value.get('PictureId', '')
-            picture_link = f'{messier_url}{picture_id}/300'
-            value['PictureLink'] = picture_link
+#         for value in list:
+#             picture_id = value.get('PictureId', '')
+#             picture_link = f'{messier_url}{picture_id}/300'
+#             value['PictureLink'] = picture_link
         
-        return jsonify(list)
-    return ''
+#         return jsonify(list)
+#     return ''
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
